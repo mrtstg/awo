edff60fe130f417985397aba3862b0cbe2a12d6b
mrtstg <mrtstgdev@gmail.com>
2026-04-15 22:26:03 +0300

    feat: added kill on drop flag to new processes, remove stdout read function

--- src/manager/process.rs
+++ src/manager/process.rs
@@ -12,24 +12,6 @@ fn split_command(command: &str) -> Vec<String> {
     arguments
 }
 
-pub async fn get_child_stdout(child: &mut Child) -> Vec<String> {
-    match child.stdout {
-        None => return Vec::new(),
-        Some(ref mut stdout) => {
-            let reader = BufReader::new(stdout);
-            let mut buffer = Vec::new();
-            let mut lines = reader.lines();
-            while let Ok(line) = lines.next_line().await {
-                match line {
-                    None => break,
-                    Some(l) => buffer.push(l),
-                }
-            }
-            return buffer;
-        }
-    }
-}
-
 pub fn build_process_from_config(data: Process) -> Result<Child, String> {
     let command = split_command(&data.command);
     if command.len() == 0 {
@@ -39,7 +21,8 @@ pub fn build_process_from_config(data: Process) -> Result<Child, String> {
     let mut cmd = &mut Command::new(command[0].clone());
     cmd.stdin(Stdio::null())
         .stdout(Stdio::piped())
-        .stderr(Stdio::piped());
+        .stderr(Stdio::piped())
+        .kill_on_drop(true);
     if command.len() > 1 {
         cmd = cmd.args(&command[1..])
     }
