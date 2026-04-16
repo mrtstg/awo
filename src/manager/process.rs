c8d4b9c6eaf028139286831776449f42496b23a1
mrtstg <mrtstgdev@gmail.com>
2026-04-16 20:59:31 +0300

    refactor: simplified process creating, removed shlex library

--- src/manager/process.rs
+++ src/manager/process.rs
@@ -1,31 +1,15 @@
 use crate::config::Process;
-use shlex::Shlex;
 use std::process::Stdio;
-use tokio::{
-    io::{AsyncBufReadExt, BufReader},
-    process::*,
-};
-
-fn split_command(command: &str) -> Vec<String> {
-    let mut arguments = Vec::new();
-    Shlex::new(command).for_each(|v| arguments.push(v));
-    arguments
-}
+use tokio::process::*;
 
 pub fn build_process_from_config(data: Process) -> Result<Child, String> {
-    let command = split_command(&data.command);
-    if command.len() == 0 {
-        return Err("Failed to parse command".to_string());
-    }
-
-    let mut cmd = &mut Command::new(command[0].clone());
+    let cmd = &mut Command::new("/bin/sh");
     cmd.stdin(Stdio::null())
         .stdout(Stdio::piped())
         .stderr(Stdio::piped())
-        .kill_on_drop(true);
-    if command.len() > 1 {
-        cmd = cmd.args(&command[1..])
-    }
+        .kill_on_drop(true)
+        .arg("-c")
+        .arg(data.command);
     match cmd.spawn() {
         Ok(child) => Ok(child),
         Err(err) => return Err(err.to_string()),
